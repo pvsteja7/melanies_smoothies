c9b77b7350eefa633f4099da63461d22a359e025
pvsteja7 <86198204+pvsteja7@users.noreply.github.com>
2026-04-09 16:00:37 +0530

    Update streamlit_app.py
    UPDATE HASH

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,9 @@ st.write("Choose the fruits you want in your custom Smoothie!")
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
 
+# Add this checkbox so you can control TRUE / FALSE
+order_filled = st.checkbox("Mark order as filled")
+
 cnx = st.connection("snowflake")
 session = cnx.session()
 
@@ -16,6 +19,7 @@ fruit_df = session.table("smoothies.public.fruit_options").select(
     col("FRUIT_NAME"),
     col("SEARCH_ON")
 )
+
 pd_df = fruit_df.to_pandas()
 
 fruit_options = pd_df["FRUIT_NAME"].tolist()
@@ -27,14 +31,14 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
-    ingredients_string = ""
+    # IMPORTANT: use join instead of adding spaces manually
+    ingredients_string = " ".join(ingredients_list)
 
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + " "
-
         st.subheader(f"{fruit_chosen} Nutrition Information")
 
         matching_rows = pd_df.loc[pd_df["FRUIT_NAME"] == fruit_chosen, "SEARCH_ON"]
+
         if not matching_rows.empty:
             search_on = matching_rows.iloc[0]
 
@@ -56,11 +60,12 @@ if ingredients_list:
 
     if time_to_insert:
         safe_name = name_on_order.replace("'", "''") if name_on_order else ""
-        safe_ingredients = ingredients_string.replace("'", "")
+        safe_ingredients = ingredients_string.replace("'", "''")
+        safe_order_filled = "TRUE" if order_filled else "FALSE"
 
         my_insert_stmt = f"""
-        insert into smoothies.public.orders(name_on_order, ingredients)
-        values ('{safe_name}', '{safe_ingredients}')
+        INSERT INTO smoothies.public.orders (name_on_order, ingredients, order_filled)
+        VALUES ('{safe_name}', '{safe_ingredients}', {safe_order_filled})
         """
 
         session.sql(my_insert_stmt).collect()
